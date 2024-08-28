cc5f44371c230581b748482fc38c49ddab4993a8
edward204 <edwardzamce@gmail.com>
2024-08-28 20:23:00 +0100

    Create logic to play a single round

--- javascript.js
+++ javascript.js
@@ -19,4 +19,44 @@ function getHumanChoice() {
     let humanChoice = prompt("Type either Rock, Paper, or Scissors:").toLowerCase();
     return humanChoice;
 }
+let humanScore = 0;
+let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You Win! Rock beats Scissors");
+        humanScore = humanScore + 1;
+
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You Win! Paper beats Rock");
+        humanScore = humanScore + 1;
+
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You Win! Scissors beats Paper");
+        humanScore = humanScore + 1;
+
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You Lose! Rock beats Scissors");
+        computerScore = computerScore + 1;
+
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You Lose! Scissors beats Paper");
+        computerScore = computerScore + 1;
+
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You Lose! Paper beats Rock");
+        computerScore = computerScore + 1;
+    }
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound (humanSelection, computerSelection);
+
+
+//console.log(getHumanChoice());
+//console.log(getComputerChoice());
